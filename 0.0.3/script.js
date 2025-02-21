907d6ed264002cfddb487ead76c26eafcc798b1f
Heron.Dev <144855270+LnXHero@users.noreply.github.com>
2025-02-21 00:08:43 -0300

    retauracao

--- 0.0.3/script.js
+++ 0.0.3/script.js
@@ -85,7 +85,7 @@ class ChatBot {
     }
 
     async getBotResponse(message) {
-        const API_KEY = 'AIzaSyDUwzHXwR3k_guPc_Htc7RImAm6y-e_wFk';
+        const API_KEY = 'API';
         const API_URL = 'https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent';
 
         console.log('Sending request to API:', API_URL);
